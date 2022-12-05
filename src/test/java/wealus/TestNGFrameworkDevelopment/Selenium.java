ca6422374adb0669f18f64004660986c1a9c0afa
Kranthi <111005858+Kranthi077@users.noreply.github.com>
2022-12-05 15:53:23 +0530

    Initial commit

---
+++ src/test/java/wealus/TestNGFrameworkDevelopment/Selenium.java
@@ -0,0 +1,57 @@
+package wealus.TestNGFrameworkDevelopment;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.internal.junit.ExactComparisonCriteria;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver d = new ChromeDriver();
+		d.manage().window().maximize();
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(d, Duration.ofSeconds(15));
+		d.get("https://rahulshettyacademy.com/client");
+		String expectedProductName = "ADIDAS ORIGINAL";
+
+		d.findElement(By.id("userEmail")).sendKeys("welcome@gmail.com");
+		d.findElement(By.id("userPassword")).sendKeys("Welcome@123");
+		d.findElement(By.id("login")).click();
+
+		List<WebElement> products = d.findElements(By.className("col-lg-4"));
+
+		for (WebElement product : products) {
+			String actualProductName = product.findElement(By.tagName("h5")).getText();
+			if (actualProductName.equalsIgnoreCase(expectedProductName)) {
+				product.findElement(By.xpath(".//button[contains(@class,'w-10')]")).click();
+				break;
+			}
+		}
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("toast-message")));
+		d.findElement(By.xpath("//button[@routerlink=\"/dashboard/cart\"]")).click();
+
+		List<WebElement> cartProducts = d.findElements(By.xpath("//div[@class=\"infoWrap\"]"));
+
+		for (WebElement cartProduct : cartProducts) {
+			String actualCartProduct = cartProduct.findElement(By.tagName("h3")).getText();
+			if (actualCartProduct.equalsIgnoreCase(expectedProductName)) {
+				cartProduct.findElement(By.className("btn-primary")).click();
+				break;
+			}
+
+		}
+	}
+
+}
